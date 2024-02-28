f6d8abfb8308ebc8c3662231f2dd894a952f4b05
winsoft666 <winsoft666@outlook.com>
2024-02-28 10:22:14 +0800

    Update copyright comment.

--- tests/message_queue_test.cpp
+++ tests/message_queue_test.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) winsoft666.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 #include <inttypes.h>
 #include <stdio.h>
 #include <time.h>
